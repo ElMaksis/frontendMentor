d43cc4a670bfeacb1836e751f2974563c3046bbf
Maks <kastrulya777@gmail.com>
2024-01-08 19:27:47 +0200

    Rest Countries - add Fetch, fix bugs

--- RestCountries/script/app.js
+++ RestCountries/script/app.js
@@ -1,112 +1,139 @@
 import refs from './refs.js';
-import data from '../data/data.json' assert { type: 'json' };
 
+function getDataFromAPI() {
+    fetch('https://restcountries.com/v3.1/all')
+        .then(response => response.json())
+        .then(data => {
 
-// (function getData() {
-//     fetch('https://restcountries.com/v3.1/all')
-//         .then(response => response.json())
-//         .then(info => console.log(info));
-// })()
+            refs.regionSelect.addEventListener('click', selectHandler);
 
+            document.querySelector('.js-search-btn').addEventListener('click', searchCountry);
+            refs.countryInput.addEventListener('input', searchCountry);
 
-refs.regionSelect.addEventListener('click', selectHandler);
+            refs.countryInput.addEventListener('keydown', (e) => {
+                if (e.code === 'Enter') searchCountry();
+            });
 
-refs.themeBtn.addEventListener('click', () => {
-    document.documentElement.classList.toggle('dark');
-});
+            refs.themeBtn.addEventListener('click', changeTheme);
 
-document.querySelector('.js-search-btn').addEventListener('click', searchCountry);
-refs.countryInput.addEventListener('input', searchCountry);
+            function selectHandler(e) {
+                e.stopImmediatePropagation();
+                refs.selectDropdaun.style.height = refs.selectDropdaun.scrollHeight + 'px';
+                refs.selectMarker.style.transform = 'rotate(180deg)';
 
-refs.countryInput.addEventListener('keydown', (e) => {
-    if (e.code === 'Enter') searchCountry();
-});
+                refs.selectList.forEach(item => {
+                    item.addEventListener('click', (e) => {
+                        e.stopImmediatePropagation();
 
-function selectHandler(e) {
-    e.stopImmediatePropagation();
-    refs.selectDropdaun.style.height = refs.selectDropdaun.scrollHeight + 'px';
-    refs.selectMarker.style.transform = 'rotate(180deg)';
+                        refs.regionSelect.querySelector('.js-select__value').textContent = item.dataset.region;
+                        refs.selectDropdaun.style.height = '0';
+                        refs.selectMarker.style.transform = 'rotate(0)';
+                        filterRegion(item.dataset.region);
+                    });
+                });
 
-    refs.selectList.forEach(item => {
-        item.addEventListener('click', (e) => {
-            e.stopImmediatePropagation();
+                document.body.addEventListener('click', (e) => {
+                    if (!e.target.classList.contains('js-select__item')) {
+                        refs.selectDropdaun.style.height = '0';
+                        refs.selectMarker.style.transform = 'rotate(0)';
+                    }
+                }, { once: true });
+            }
 
-            refs.regionSelect.querySelector('.js-select__value').textContent = item.dataset.region;
-            refs.selectDropdaun.style.height = '0';
-            refs.selectMarker.style.transform = 'rotate(0)';
-            filterRegion(item.dataset.region);
+            function filterRegion(region) {
+                const filterList = data.filter(item => item.region === region);
+
+                renderCountryList(filterList);
+            }
+
+            function searchCountry() {
+                let inputData = refs.countryInput.value.trim().toLowerCase();
+
+                const currentResults = data.filter(item => item.name.common.toLowerCase().includes(inputData));
+
+                renderCountryList(currentResults);
+            }
+
+            function renderCountryList(data) {
+                refs.countryBox.innerHTML = '';
+
+                data.forEach(item => {
+                    const newCountry = document.createElement('div');
+
+                    newCountry.classList.add('js-country-item',
+                        'country__item');
+                    newCountry.innerHTML =
+                        `<a href="detail.html?country=${item.cioc}">
+                            <div>
+                                <img class="country__img" src="${item.flags.svg}" alt="${item.name.common}">
+                            </div>
+                            <div class="country__content">
+                                <h3 class="js-country__name country__title">${item.name.common}</h3>
+                                <ul class="country__list">
+                                    <li class="country__list-item">
+                                        <span>Population:</span>
+                                        <span class="js-country-population country__output">${new Intl.NumberFormat("ru-RU").format(item.population)}</span>
+                                    </li>
+                                    <li class="country__list-item">
+                                        <span>Region:</span>
+                                        <span class="js-country-region country__output">${item.region}</span>
+                                    </li>
+                                    <li class="country__list-item">
+                                        <span>Capital:</span>
+                                        <span class="js-country-capital country__output">${item.capital}</span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </a>`;
+
+                    refs.countryBox.insertAdjacentElement('beforeend', newCountry);
+                });
+            }
+
+            function changeTheme() {
+                if (document.documentElement.classList.contains('dark')) {
+                    document.documentElement.classList.remove('dark');
+                    localStorage.setItem('RESTCountryTheme', 'light');
+                } else {
+                    document.documentElement.classList.add('dark');
+                    localStorage.setItem('RESTCountryTheme', 'dark')
+                }
+            }
+
+            function initialPage() {
+                const params = new URLSearchParams(window.location.search);
+                const selectRegion = params.get('region');
+
+                if (selectRegion) {
+                    filterRegion(selectRegion);
+                    refs.regionSelect.querySelector('.js-select__value').textContent = selectRegion;
+                } else {
+                    renderCountryList(data);
+                }
+            }
+
+            initialPage();
         });
-    });
-
-    document.body.addEventListener('click', (e) => {
-        if (!e.target.classList.contains('js-select__item')) {
-            refs.selectDropdaun.style.height = '0';
-            refs.selectMarker.style.transform = 'rotate(0)';
-        }
-    }, { once: true });
 }
 
-function filterRegion(region) {
-    const filterList = data.filter(item => item.region === region);
+getDataFromAPI();
 
-    renderCountryList(filterList);
-}
+(function setTheme() {
+    const myTheme = localStorage.getItem('RESTCountryTheme');
 
-function searchCountry() {
-    let inputData = refs.countryInput.value.trim().toLowerCase();
-
-    const currentResults = data.filter(item => item.name.toLowerCase().includes(inputData));
-
-    renderCountryList(currentResults);
-}
-
-function renderCountryList(data) {
-    refs.countryBox.innerHTML = '';
-
-    data.forEach(item => {
-        const newCountry = document.createElement('div');
-
-        newCountry.classList.add('js-country-item',
-            'country__item');
-        newCountry.innerHTML =
-            `<a href="detail.html?country=${item.alpha3Code}">
-                <div>
-                    <img class="country__img" src="${item.flag}" alt="${item.name}">
-                </div>
-                <div class="country__content">
-                    <h3 class="js-country__name country__title">${item.name}</h3>
-                    <ul class="country__list">
-                        <li class="country__list-item">
-                            <span>Population:</span>
-                            <span class="js-country-population country__output">${item.population}</span>
-                        </li>
-                        <li class="country__list-item">
-                            <span>Region:</span>
-                            <span class="js-country-region country__output">${item.region}</span>
-                        </li>
-                        <li class="country__list-item">
-                            <span>Capital:</span>
-                            <span class="js-country-capital country__output">${item.capital}</span>
-                        </li>
-                    </ul>
-                </div>
-            </a>`;
-
-        refs.countryBox.insertAdjacentElement('beforeend', newCountry);
-    });
-}
-
-function initialPage() {
-    const params = new URLSearchParams(window.location.search);
-    const selectRegion = params.get('region');
-
-    if (selectRegion) {
-        filterRegion(selectRegion);
-        refs.regionSelect.querySelector('.js-select__value').textContent = selectRegion;
-    } else {
-        renderCountryList(data);
+    if (myTheme === 'dark') {
+        document.documentElement.classList.remove('light');
+        document.documentElement.classList.add('dark');
     }
-}
 
-initialPage();
+    if (myTheme === 'light') {
+        document.documentElement.classList.remove('dark');
+        document.documentElement.classList.add('light');
+    }
+}())
+
+
+
+
+
 
--- RestCountries/script/detail.js
+++ RestCountries/script/detail.js
@@ -1,47 +1,74 @@
 import refs from './refs.js';
-import data from '../data/data.json' assert { type: 'json' };
 
-findCountry();
+fetchCountry();
 
-refs.themeBtn.addEventListener('click', () => {
-    document.documentElement.classList.toggle('dark');
-});
+function fetchCountry() {
+    fetch(`https://restcountries.com/v3.1/alpha/${parsUrl()}`)
+        .then(response => response.json())
+        .then(country => {
 
-function findCountry() {
-    const params = new URLSearchParams(window.location.search),
-        countryCode = params.get('country');
-    const currentCountry = data.find(item => item.alpha3Code === countryCode);
+            function renderPage() {
 
-    renderPage(currentCountry);
-    setRegion(currentCountry.region);
+                refs.detailFlag.src = country[0].flags.svg;
+                refs.detailCountryTitle.textContent = country[0].name.common || '--';
+                refs.detailCountryName.textContent = Object.values(country[0].name.nativeName)[0].common || '--';
+                refs.detailPopulation.textContent = new Intl.NumberFormat("ru-RU").format(country[0].population) || '--';
+                refs.detailRegion.textContent = country[0].region || '--';
+                refs.detailSubRegion.textContent = country[0].subregion || '--';
+                refs.detailCapital.textContent = country[0].capital || '--';
+                refs.detailDomain.textContent = country[0].tld ? country[0].tld.join(',') : '--';
+                refs.detailCurrency.textContent = country[0].currencies ? Object.values(country[0].currencies).map(item => item.name).join(', ') : '--';
+                refs.detailLang.textContent = country[0].languages ? Object.values(country[0].languages).join(', ') : '--';
+
+                if (country[0].borders) {
+                    country[0].borders.forEach(item => {
+
+                        refs.detailNeighborBox.insertAdjacentHTML('beforeend',
+                            `<a class="neighbor-item py-1 px-6 text-xs sm:text-sm dark:bg-[#2B3844] rounded-sm
+                        shadow-light hover:bg-[#ebebf3] dark:hover:bg-[#253542] hover:scale-105 transition-all"
+                        href="detail.html?country=${item}">${item}</a>`)
+                    });
+                }
+            }
+
+            function setRegion(region) {
+                refs.backBtn.href = `index.html?region=${region}`;
+            }
+
+            setRegion(country[0].region);
+            renderPage();
+        });
 }
 
-function renderPage(country) {
-    refs.detailFlag.src = country.flag;
-    refs.detailCountryTitle.textContent = country.name || '--';
-    refs.detailCountryName.textContent = country.nativeName || '--';
-    refs.detailPopulation.textContent = country.population || '--';
-    refs.detailRegion.textContent = country.region || '--';
-    refs.detailSubRegion.textContent = country.subregion || '--';
-    refs.detailCapital.textContent = country.capital || '--';
-    refs.detailDomain.textContent = country.topLevelDomain ? country.topLevelDomain.join(',') : '--';
-    refs.detailCurrency.textContent = country.currencies ? country.currencies.map(item => item.name).join(', ') : '--';
-    refs.detailLang.textContent = country.languages ? country.languages.map(item => item.name).join(', ') : '--';
+refs.themeBtn.addEventListener('click', changeTheme);
 
-    if (country.borders) {
-        country.borders.forEach(border => {
-            const neighbor = data.find(country => country.alpha3Code === border);
+initialPage();
 
-            refs.detailNeighborBox.insertAdjacentHTML('beforeend',
-                `<a class="neighbor-item py-1 px-6 text-xs sm:text-sm dark:bg-[#2B3844] rounded-sm 
-            shadow-light hover:bg-[#ebebf3] dark:hover:bg-[#253542] hover:scale-105 transition-all"
-            href="detail.html?country=${neighbor.alpha3Code}">${neighbor.name}</a>`)
-        });
+function parsUrl() {
+    const params = new URLSearchParams(window.location.search);
+
+    return params.get('country');
+}
+
+function initialPage() {
+    const favTheme = localStorage.getItem('RESTCountryTheme');
+
+    if (favTheme) {
+        document.documentElement.classList.add(favTheme);
     }
 }
 
-function setRegion(region) {
-    refs.backBtn.href = `index.html?region=${region}`;
+function changeTheme() {
+    if (document.documentElement.classList.contains('dark')) {
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem('RESTCountryTheme', 'light');
+    } else {
+        document.documentElement.classList.add('dark');
+        localStorage.setItem('RESTCountryTheme', 'dark')
+    }
 }
+
+
+
 
 
